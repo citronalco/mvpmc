f8b96a44bcbae8392b2b0c21c179d529923c2313
mvpmc <mvpmc@mvpmc.(none)>
2014-07-26 06:12:05 -0400

    2014-05-09 Fix cmyth_file_destroy() so the file_control mutex can be destroyed. [gettler]

--- libs/libcmyth/file.c
+++ libs/libcmyth/file.c
@@ -74,8 +74,8 @@ cmyth_file_destroy(cmyth_file_t file)
 			goto fail;
 		}
 	    fail:
-		ref_release(file->file_control);
 		pthread_mutex_unlock(&file->file_control->conn_mutex);
+		ref_release(file->file_control);
 	}
 	if (file->closed_callback) {
 		(file->closed_callback)(file);
